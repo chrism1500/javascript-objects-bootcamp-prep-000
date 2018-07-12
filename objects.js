b4797c6d82d0f46dec58588e5843b9ba102d9159
morales.chris1989@gmail.com <morales.chris1989@gmail.com>
2018-07-12 17:24:49 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,11 @@
-var playlist = new Object
-  artist:
-  titles:
-  
-  
+
+var playlist = new Object({keys: '0'});
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+return playlist
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist}
